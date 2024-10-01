d02dbce78c75806abb314b054a28cf301e5b8067
DuesanNoften <alejandre030902@gmail.com>
2024-09-30 21:37:46 -0600

    Se realizaron cambions en CEthread_create

--- CEthreads/CEthread_create.c
+++ CEthreads/CEthread_create.c
@@ -1,15 +1,27 @@
 //
 // Created by duesan on 30/09/24.
 //
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct {
+    int (*start_routine)(void *);
+    void *arg;
+    char *stack;
+    size_t stack_size;
+} CEthreads_create;
 
 #include "CEthread.h"
-int CEthread_create(CEthread *thread, void *(*start_routine)(void *), void *arg){
-  CEthread_attr_t attr;
-  CEthread_attr_init(&attr);
-  CEthread_attr_setdetachstate(&attr, CETHREAD_CREATE_JOINABLE);
-  CEthread_create(&thread->id, &attr, start_routine, arg);
-  CEthread_attr_destroy(&attr);
-  return 0;
-  }
+int CEthread_create(CEthreads_create *ce_thread){
+    ce_thread->stack = malloc(ce_thread->stack_size);
+    if (ce_thread->stack == NULL) {
+        return -1;
+    }
+    return clone((int (*)(void *))ce_thread->start_routine, ce_thread->stack + ce_thread->stack_size,
+                 CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD, ce_thread->arg);
+}
 
 
